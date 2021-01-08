96366539c316bd11d6cb091e52d65c8deddba1f7
Evgeny Ageev <ageev@reksoft.ru>
2021-01-09 02:03:43 +0300

    Bash fix

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -7,12 +7,6 @@ resource "aws_instance" "example" {
 	instance_type = "t2.micro"
 	vpc_security_group_ids = [aws_security_group.instance.id]
 
-	user_data = <<-EOF
-				#!/bin/bash
-				echo "Hello, world" > index.html
-				nohup busybox httpd -f -p ${var.server_port} &
-				EOF
-
 	tags = {
 		Name = "terraform-example"
 	}
--- stage/services/webserver-cluster/user-data.sh
+++ stage/services/webserver-cluster/user-data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cat > index.html <<EOF
-<h1>"Hello, world"</h1>
-<p style="color: green">DB Address: ${db_address}</p>
-<p style="color: green>DB Port: ${db_port}</p>
+<h1>Hello, world</h1>
+<p style="color: 0000FF">DB Address: ${db_address}</p>
+<p style="color: 0000FF">DB Port: ${db_port}</p>
 EOF
 
 nohup busybox httpd -f -p ${server_port} &
